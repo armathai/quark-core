3fceb03723cbb5b64a8309323fb2e2bf8cafb3f3
saqsun <sargsyan.sargis.89@gmail.com>
2021-09-15 17:24:36 +0400

    minor gravity effect refactor

--- src/core/effect/Gravity.ts
+++ src/core/effect/Gravity.ts
@@ -2,7 +2,7 @@ import { EaseFunction } from '../../ease/index';
 import { Force } from './Force';
 
 export class Gravity extends Force {
-    public constructor(g: number, life: number, easing: string | EaseFunction) {
+    public constructor(g: number, life?: number, easing?: string | EaseFunction) {
         super(0, g, life, easing);
     }
 }
